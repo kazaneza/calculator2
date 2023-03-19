ebef4fbd73feaadee9891e0ff9b4a65f5805bfaf
kazaneza <kazanezagentil@gmail.com>
2023-03-19 15:57:35 +0100

    created the home basic styilng

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='inner'>
+          <div className='result'>
+            <div className='resbox'></div>
+          </div>
+        </div>
+        <div className='btns'></div>
+    </div>
+  )
+}
+
+export default Home
