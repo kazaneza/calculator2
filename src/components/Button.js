ac4792fe6189991ef8f97b4b786a0616523d4f9f
kazaneza <kazanezagentil@gmail.com>
2023-03-19 22:57:44 +0100

    added the condition for c and =

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,7 @@ import classes from './Button.module.css'
 
 function Button({value}) {
   return (
+    //added condition to check if c and = are contained and we apply two class else if not contained we apply on class
     <div className={value == "c" || value == "=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}`}>
         {value}
     </div>
