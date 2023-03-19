e336d5e6aa5aa6aeb37ea61a04d964a2b1527969
kazaneza <kazanezagentil@gmail.com>
2023-03-19 22:58:36 +0100

    added buttons and buttons styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import classes from './Home.module.css';
+import classes from './Home.module.css'
+import Button from './Button'
+
 
 function Home() {
+
+  const buttons = ["c","9","/","8", "7", "6","*","5","4","3","+","2","1","0","-",".","DEL","="];
   return (
     <div className={classes.home}>
         <div className={classes.inner}>
           <div className={classes.result}>
             <div className={classes.resbox}></div>
           </div>
+        
+        <div className={classes.btns}>
+          {buttons.map((ele,index) => {return <Button value={ele} key={index} />})}
         </div>
-        <div className={classes.btns}></div>
+        </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
