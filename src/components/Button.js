d5359228a835dddb4f62e104d224f2673a4fe1fc
kazaneza <kazanezagentil@gmail.com>
2023-03-22 03:56:26 +0100

    added handler prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import classes from './Button.module.css'
 
-function Button({value}) {
+function Button({value, handler}) {
   return (
     //added condition to check if c and = are contained and we apply two class else if not contained we apply on class
-    <div className={value == "c" || value == "=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}`}>
+    <div className={value == "c" || value == "=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}`} onClick = {() => handler (value)}>
         {value}
     </div>
   )
